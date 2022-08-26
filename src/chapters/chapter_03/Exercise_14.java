a46af052206f9bd016d459a4d1e5e0553dac9a0a
Serhat Gulbas <sgulbas525@gmail.com>
2022-08-26 23:58:18 +0300

    Added some exercises of chapter3

---
+++ src/chapters/chapter_03/Exercise_14.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercise_14{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+
+		int coin = (int)(Math.random()*2);
+
+		System.out.print("Heads(0) or Tails(1) ? ");
+		int guess = input.nextInt();
+
+		if(coin == guess)
+			System.out.println("You are correct");
+		else
+			System.out.println("You are incorrect. Try again pls");
+	}
+}
