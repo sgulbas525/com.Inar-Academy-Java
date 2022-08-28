341b72b0805ade138c06412a847574c0a37e0224
Serhat Gulbas <sgulbas525@gmail.com>
2022-08-28 23:12:53 +0300

    Completed Chapters 3's exercises

---
+++ src/chapters/chapter_03/Exercise_34.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercise_34{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("ENter three points for p1, p2, p3: ");
+		double x1 = input.nextDouble();
+		double y1 = input.nextDouble();
+		double x2 = input.nextDouble();
+                double y2 = input.nextDouble();
+		double x3 = input.nextDouble();
+                double y3 = input.nextDouble();
+
+		System.out.print("(" + x3 + ", " + y3 +") is ");
+		
+		if((x3 > x1 && x3 < x2) && (y3 > y1 && y3 < y2))
+			System.out.print("");
+		else
+			System.out.print("not");
+
+		System.out.println(" on the line segment from (" + x1 + ", " + y1 + ") to (" + x2 + ", " + y2 + ")");
+	}
+}
+
