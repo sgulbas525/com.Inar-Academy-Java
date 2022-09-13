2cd459569e8363c2274e9347f7352f1e5fa4f039
Serhat Gulbas <sgulbas525@gmail.com>
2022-09-14 00:12:40 +0300

    Revised chapter 4 Exercise 7

--- src/chapters/chapter_04/Exercise04_07.java
+++ src/chapters/chapter_04/Exercise04_07.java
@@ -6,21 +6,21 @@ public class Exercise04_07 {
         double radius = input.nextDouble();
         double angle = 0 ;
 
-        double x1 = radius * (Math.cos(Math.toRadians(angle)));
-        double y1 = radius * (Math.sin(Math.toRadians(angle)));
-        angle += 72;
-        double x2 = radius * (Math.cos(Math.toRadians(angle)));
-        double y2 = radius * (Math.sin(Math.toRadians(angle)));
-        angle += 72;
-        double x3 = radius * (Math.cos(Math.toRadians(angle)));
-        double y3 = radius * (Math.sin(Math.toRadians(angle)));
-        angle += 72;
-        double x4 = radius * (Math.cos(Math.toRadians(angle)));
-        double y4 = radius * (Math.sin(Math.toRadians(angle)));
-        angle += 72;
-        double x5 = radius * (Math.cos(Math.toRadians(angle)));
-        double y5 = radius * (Math.sin(Math.toRadians(angle)));
-//Code is wrong I'll work on it later.
+        double x1 = radius * Math.cos(angle);
+        double y1 = radius * Math.sin(angle);
+        angle += 2 * Math.PI / 5;
+        double x2 = radius * Math.cos(angle);
+        double y2 = radius * Math.sin(angle);
+        angle += 2 * Math.PI / 5;
+        double x3 = radius * Math.cos(angle);
+        double y3 = radius * Math.sin(angle);
+        angle += 2 * Math.PI / 5;
+        double x4 = radius * Math.cos(angle);
+        double y4 = radius * Math.sin(angle);
+        angle += 2 * Math.PI / 5;
+        double x5 = radius * Math.cos(angle);
+        double y5 = radius * Math.sin(angle);
+
         System.out.println("The coordinates of five points on the pentagon area\n(" + x1 + ", " + y1 + ")\n(" + x2 + ", " + y2 + ")\n(" + x3 + ", " + y3 + ")\n(" + x4 + ", " + y4 + ")\n(" + x5 + ", " + y5 + ")\n");
     }
 }
