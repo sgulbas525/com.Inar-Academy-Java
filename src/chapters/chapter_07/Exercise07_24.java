e44c951dadfb33ceab3cd6b4dc845e8ada9767ae
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-03 19:00:53 +0300

    Exercises of Chapter 7 has been done

--- src/chapters/chapter_07/Exercise07_24.java
+++ src/chapters/chapter_07/Exercise07_24.java
@@ -6,45 +6,45 @@ public class Exercise07_24 {
     }
 
     public static void playGame() {
-        int[] minePick = new int[4];
+        int[] myPick = new int[4];
         int countPick = 0;
 
-        for (int i = 0; i < minePick.length; i++) {
+        for (int i = 0; i < myPick.length; i++) {
             while (true) {
-                pickCard(minePick, i);
+                pickCard(myPick, i);
                 countPick++;// Count total pick
-                if (isValid(minePick, i)) {
+                if (isValid(myPick, i)) {
                     break;// if pick is valid
                 }
             }
         }
 
-        displayMineDeck(minePick, countPick);
+        displayMineDeck(myPick, countPick);
     }
 
-    public static boolean isValid(int[] minePick, int i) {
+    public static boolean isValid(int[] myPick, int i) {
         for (int j = 0; j < i; j++) {
           // Compares the last pick with the card that we already picked
-            if ((minePick[j] / 13) == (minePick[i] / 13)){
+            if ((myPick[j] / 13) == (myPick[i] / 13)){
                 return false;
             }
         }
         return true;
     }
 
-    public static void displayMineDeck(int[] minePick, int countPick) {
+    public static void displayMineDeck(int[] myPick, int countPick) {
         String[] suits = {"Spades", "Heart", "Diamonds", "Clubs"};
         String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
         System.out.println("Total Pick: " + countPick);
 
-        for (int i = 0; i < minePick.length ; i++) {
-            System.out.println(ranks[minePick[i] % 13] + " of " + suits[minePick[i] / 13]);
+        for (int i = 0; i < myPick.length ; i++) {
+            System.out.println(ranks[myPick[i] % 13] + " of " + suits[myPick[i] / 13]);
         }
     }
 
 
-    public static void pickCard(int[] mineDeck, int i) {
-        mineDeck[i] = (int)(Math.random() * 52);
+    public static void pickCard(int[] myDeck, int i) {
+        myDeck[i] = (int)(Math.random() * 52);
     }
 }
---
+++ src/chapters/chapter_07/Exercise07_35.java
@@ -0,0 +1,100 @@
+package chapters.chapter_07;
+
+import java.util.Scanner;
+
+public class Exercise07_35 {
+    public static void main(String[] args) {
+
+        String[] words = {"Besiktas", "GoldenState", "SeaHawks", "Yankees", "ManchesterCity", "Barcelona", "Liverpool"};
+        playHangman(words);
+
+    }
+
+    public static void playHangman(String[] words) {
+        Scanner input = new Scanner(System.in);
+        String word ;
+
+        do {
+            // In each game a new word
+            word = words[(int) (Math.random() * words.length)];
+            char[] answer = new char[word.length()];
+            askToUser(word, answer);
+
+            System.out.print("Do you want to guess another word? Enter y or n>");
+        } while (input.next().toLowerCase().charAt(0) == 'y');
+    }
+
+    public static void askToUser(String word, char[] answer) {
+        Scanner input = new Scanner(System.in);
+        fill(answer, '*');
+        int missTime = 0;
+
+        while (true) {
+            System.out.print("(Guess) Enter a letter in word ");
+            printTheWordInAsterisk(answer);
+
+            char ch = input.next().toLowerCase().charAt(0);// for case-sensitive
+            missTime += checkLetter(word, answer, ch);
+
+            if (checkAnswer(word, answer)) {
+                break;
+            }
+        }
+        System.out.println("The word is " + word);
+        if (missTime == 0) {
+            System.out.println("You didn't miss");
+        } else if (missTime == 1) {
+            System.out.println("You missed " + missTime + " time");
+        } else {
+            System.out.println("You missed " + missTime + " times");
+        }
+    }
+
+    // Checks the letter whether is in the word
+    public static int checkLetter(String word, char[] answer, char ch) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.toLowerCase().charAt(i) == ch) {
+                answer[i] = word.charAt(i);
+            }
+        }
+
+        return contains(answer, ch) ? 1 : 0;// counts miss time
+
+    }
+
+    // Check whether the input letter has been entered
+    public static boolean contains(char[] answer, char ch) {
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i] == ch) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // Checks whether the user has find the word
+    private static boolean checkAnswer(String word, char[] answer) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != answer[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Prints known-letter and asterisks
+    private static void printTheWordInAsterisk(char[] answer) {
+        for (int i = 0; i < answer.length; i++) {
+            System.out.print(answer[i]);
+        }
+        System.out.print(" > ");
+    }
+
+    // Fills array with asterisks
+    public static void fill(char[] answer, char ch) {
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = ch;
+        }
+    }
+}
