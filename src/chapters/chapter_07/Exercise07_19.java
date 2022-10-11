e982deefcb4b64072ecec4dd660259b727ceed60
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-11 21:54:01 +0300

    Revised Exercise07_19

--- src/chapters/chapter_07/Exercise07_19.java
+++ src/chapters/chapter_07/Exercise07_19.java
@@ -12,7 +12,8 @@ public class Exercise07_19 {
     public static void displayListWhetherSorted(int[] list) {
         System.out.print("The list is");
         // Conditional Expressions                 for true       for false
-        System.out.println((isSorted(list) ? " already sorted": " not sorted"));
+        System.out.println((isTheArraySortedFromGreaterToSmaller(list) || isTheArraySortedFromSmallerToGreater(list) ?
+                " already sorted": " not sorted"));
     }
 
     public static int[] getListFromUser() {
@@ -28,14 +29,25 @@ public class Exercise07_19 {
         return list;
     }
 
-    public static boolean isSorted(int[] list) {
-        // Checking with next index, so the loop must be up to length - 1
-        for (int i = 0; i < list.length - 1; i++) {
-            if (list[i] > list[i + 1]) //
+    //100 90 80 70
+    public static boolean isTheArraySortedFromGreaterToSmaller(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] < arr[i + 1]){
                 return false;
+            }
         }
         return true;
     }
 
+    // 42 5 8 96 31 475 16 45
+    // 15 23 13 46 58 67
+    public static boolean isTheArraySortedFromSmallerToGreater(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]){
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
