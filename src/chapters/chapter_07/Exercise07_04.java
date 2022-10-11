10b608daa0f5b2e18a1d0923bc25e0f18bca73ec
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-11 10:54:43 +0300

    revised exercise07_04

--- src/chapters/chapter_07/Exercise07_04.java
+++ src/chapters/chapter_07/Exercise07_04.java
@@ -1,13 +1,41 @@
 package chapters.chapter_07;
 
+import java.util.Scanner;
+
 public class Exercise07_04 {
     public static void main(String[] args) {
 
-        analyzeScore(1, 5, 79, 25, 15, 75, 68, -1);
+        double[] arr = getFromUser();
+        analyzeScore(arr);
+
 
     }
 
-    public static void analyzeScore(double... scores) {
+    public static double[] getFromUser() {
+        Scanner input = new Scanner(System.in);
+        double[] numbers = new double[50];
+        int index = 0;
+        double number = 0;
+        System.out.print("Enter the integers between 0 and 100 (negative ends): ");
+
+        do {
+            number = input.nextInt();
+            // For not assigning 0 to an index
+            if (number < 0) {
+                numbers[index] = number;
+                index++;
+            }
+
+            // If the array is full, but we are still taking input we must enlarge the size of our array
+            if (index == numbers.length) {
+                numbers = resizeArray(numbers);
+            }
+        } while (number < 0);
+
+        return numbers;
+    }
+
+    public static void analyzeScore(double[] scores) {
         int average = average(scores);
         int countAboveOrEqual = 0;
         int countBelow = 0;
@@ -32,6 +60,10 @@ public class Exercise07_04 {
         int count = 0;
 
         for (int i = 0; i < scores.length; i++) {
+            if (scores[i] < 0) {
+                break;
+            }
+
             if (scores[i] >= 0) {
                 sum += scores[i];
                 count++;
@@ -41,5 +73,13 @@ public class Exercise07_04 {
         System.out.println("The average is " + average);
 
         return average;
+    }
+
+    public static double[] resizeArray(double[] numbers) {
+        double[] newArray = new double[numbers.length * 2];
+        java.util.Arrays.fill(newArray, -1);
+        // Copied the full array's elements into our new resized array
+        System.arraycopy(numbers, 0, newArray, 0, numbers.length);
+        return newArray;
     }
 }
