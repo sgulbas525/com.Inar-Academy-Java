e43c166e2bd3fd8d7b82c5dd090cf3b9773d7269
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-11 12:28:11 +0300

    Revised exercise07_12

--- src/chapters/chapter_07/Exercise07_12.java
+++ src/chapters/chapter_07/Exercise07_12.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class Exercise07_12 {
     public static void main(String[] args) {
+        double[] list = getTenNumbersFromUser();
+
+        System.out.println("Before reverse method:");
+        displayArray(list);
+
+        reverse(list);
+
+        System.out.println("\nAfter reverse method:");
+        displayArray(list);
+
+    }
+
+    public static double[] getTenNumbersFromUser() {
         Scanner input = new Scanner(System.in);
         final int NUMBER_OF_NUMBERS = 10;
 
@@ -13,22 +26,17 @@ public class Exercise07_12 {
         for (int i = 0; i < list.length; i++) {
             list[i] = input.nextDouble();
         }
-        System.out.println("Before reverse method:");
-        displayArray(list);
-        reverse(list);
-        System.out.println("\nAfter reverse method:");
-        displayArray(list);
-
+        return list;
     }
 
     public static void reverse(double[] list) {
         double temp;
        // Change first-last, 2nd-oneBeforeLast, and it goes like that
         // Working on the same array
-        for (int i = 0, j = list.length - 1; i < j; i++, j--) {
-            temp = list[i];
-            list[i] = list[j];
-            list[j] = temp;
+        for (int firstIndex = 0, lastIndex = list.length - 1; firstIndex < lastIndex; firstIndex++, lastIndex--) {
+            temp = list[firstIndex];
+            list[firstIndex] = list[lastIndex];
+            list[lastIndex] = temp;
         }
     }
 
