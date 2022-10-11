aaa5f0509bd8e0d0fff955095d52f82c916ac3f3
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-11 15:02:34 +0300

    Added isUniqueWithArray

---
+++ src/weeks/week_09/isUniqueWithArrays.java
@@ -0,0 +1,33 @@
+package weeks.week_09;
+
+import java.util.Scanner;
+
+public class isUniqueWithArrays {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a string value :");
+        String s = input.nextLine();
+        boolean result = isUnique(s);
+        System.out.println(result);
+
+    }
+
+    public static boolean isUnique(String s) {
+        int counter = 0;
+        boolean[] myAsciiTable = new boolean[128];
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i);
+            counter++;
+            if (myAsciiTable[index]) {
+                System.out.println("This is the step number :" + counter);
+                return false;
+            } else {
+                myAsciiTable[index] = true;
+            }
+        }
+        System.out.println("This is the step number :" + counter);
+        return true;
+
+    }
+}
