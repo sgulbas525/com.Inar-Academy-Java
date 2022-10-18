96f24b619a2439b1a9dc96c906b9ec6f8b2c5a15
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-18 23:25:06 +0300

    Added chapter9's checkpoints

---
+++ src/chapters/chapter_09/Checkpoints09/Checkpoint09_01_02_03_04.java
@@ -0,0 +1,35 @@
+package chapters.chapter_09.Checkpoints09;
+
+public class Checkpoint09_01_02_03_04 {
+    public static void main(String[] args) {
+
+        // Checkpoint09_01
+        /* An Object is an instance of that class. Class is the receipt of an instance. */
+
+
+
+
+
+        // Checkpoint09_02
+        /* A class is a template, blueprint,
+        or contract that defines what an object’s data fields and methods will be. */
+
+
+
+
+        // Checkpoint09_03
+        /* We declare an object by his class name (i.e : ClassName instanceVariableName;
+         */
+
+
+
+
+        // Checkpoint09_04
+        /* After declaration we create an instance by using its class constructor with the keyword new
+        (i.e : ClassName instanceVariableName = new ClassName();
+         */
+
+
+    }
+
+}
