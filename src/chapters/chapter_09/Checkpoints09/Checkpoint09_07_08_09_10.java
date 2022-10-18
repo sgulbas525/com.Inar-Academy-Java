9de68d1c79c25657276911af66e66fcb09b0d71f
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-18 23:26:05 +0300

    Added chapter9's checkpoints

---
+++ src/chapters/chapter_09/Checkpoints09/Checkpoint09_07_08_09_10.java
@@ -0,0 +1,31 @@
+package chapters.chapter_09.Checkpoints09;
+
+public class Checkpoint09_07_08_09_10 {
+    public static void main(String[] args) {
+
+        // Checkpoint09_07
+        /* The dot(.) operator is used to access a data field or to invoke a method */
+
+
+
+        // Checkpoint09_08
+        /* If an object(instance) is created but not assigned to a variable,
+         it's called anonymous object
+          */
+
+
+
+
+        // Checkpoint09_09
+        /* If we invoke a method by using a reference variable that reference 'null',
+        it'll cause a NullPointerException
+         */
+
+
+
+        // Checkpoint09_10
+        /* An array is an object. Yes an array can contain objects. The default values are changed by the array's type.
+        (i.e : For the number types the default values is 0, for char it's \u0000, for string it's null.
+         */
+    }
+}
