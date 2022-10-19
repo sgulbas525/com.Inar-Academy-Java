868bbdd5111752bcb986d3ddbc9ce61599daca8e
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-19 08:09:57 +0300

    Revised Exercise07_29

---
+++ src/chapters/chapter_07/Exercise07_29.java
@@ -0,0 +1,47 @@
+package chapters.chapter_07;
+
+public class Exercise07_29 {
+    public static void main(String[] args) {
+        playGame();
+    }
+
+    public static void playGame() {
+
+        int[] minePicks = pickCards();
+        displayMyPicks(minePicks);
+    }
+
+    public static int[] pickCards() {
+        int sum = 0;
+        int[] myPick = new int[5];
+        int counter = 0;
+
+        while (sum != 24) {
+            for (int i = 0; i < myPick.length - 1; i++) {
+                myPick[i] = (int) (Math.random() * 52);
+            }
+            sum = getSum(myPick);
+            counter++;
+        }
+        return myPick;
+    }
+
+    public static int getSum(int[] myPick) {
+        int sum = 0;
+        for (int i = 0; i < myPick.length - 1; i++) {
+            sum += myPick[i] % 13 == 0 ? 13 : myPick[i] % 13;
+        }
+        return sum;
+    }
+
+    public static void displayMyPicks(int[] myPick) {
+        String[] suits = {"Spades", "Heart", "Diamonds", "Clubs"};
+        String[] ranks = {"King", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen"};
+
+        for (int i = 0; i < myPick.length - 1; i++) {
+            System.out.println(ranks[myPick[i] % 13] + " of " + suits[myPick[i] / 13]);
+        }
+
+        System.out.println("Enter you have " + myPick[4] + " times picked for reaching 24");
+    }
+}
