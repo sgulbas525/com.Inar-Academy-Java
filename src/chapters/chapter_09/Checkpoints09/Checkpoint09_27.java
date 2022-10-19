1e65bd8feb403257bba060cbc2b8b4ec8a6581ad
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-19 12:48:48 +0300

    Added Chapter9's Checkpoints

---
+++ src/chapters/chapter_09/Checkpoints09/Checkpoint09_27.java
@@ -0,0 +1,10 @@
+package chapters.chapter_09.Checkpoints09;
+
+public class Checkpoint09_27 {
+    public static void main(String[] args) {
+        java.util.Date[] dates = new java.util.Date[10];
+        System.out.println(dates[0]);
+        //System.out.print(dates[0].toString());
+        // If we invoke a method via a null valued variable it causes a NullPointerException
+    }
+}
