42ba11fc31c76f89d31fc4ec424aa84029d8c889
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-19 20:26:03 +0300

    Added some Checkpoints of Chapter10

---
+++ src/chapters/chapter_10/Checkpoints10/Checkpoint10_01.java
@@ -0,0 +1,8 @@
+package chapters.chapter_10.Checkpoints10;
+
+public class Checkpoint10_01 {
+
+    /*
+    it wouldn't because getLoanDate returns the reference of the private data field.
+     */
+}
