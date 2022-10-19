395b258f99a4c3a8940660873b3b72fff0e27b7e
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-19 15:55:49 +0300

    Revised Listing07_08_SelectionSort

--- src/chapters/chapter_07/Listings07/Listing07_08_SelectionSort.java
+++ src/chapters/chapter_07/Listings07/Listing07_08_SelectionSort.java
@@ -34,4 +34,26 @@ public class Listing07_08_SelectionSort {
             System.out.print(list[i] + "  ");
         }
     }
+
+
+    public static void selectionSort(int[] list) {
+
+        for (int i = 0; i < list.length - 1; i++) {
+            int currentMin = list[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMinIndex = j;
+                    currentMin = list[j];
+                }
+            }
+
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+
+    }
 }
