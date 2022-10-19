d807237d22b2c96ab120a689794e5d3397570023
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-19 15:53:02 +0300

    Added Chapter_09's Exercises

---
+++ src/chapters/chapter_09/Exercise09_03.java
@@ -0,0 +1,15 @@
+package chapters.chapter_09;
+
+import java.util.Date;
+
+public class Exercise09_03 {
+    public static void main(String[] args) {
+        Date date = new Date();
+
+        for (int i = 4; i < 12; i++) {
+            date.setTime((long)(Math.pow(10,i)));
+            System.out.println("For elapsed " + (long)(Math.pow(10, i)) +
+                    " time is " + date.toString());
+        }
+    }
+}
