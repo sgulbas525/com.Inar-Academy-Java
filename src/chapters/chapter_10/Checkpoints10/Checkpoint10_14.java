51e8a54ee44cbfcc68ac3cc2ef7f8f486b72b226
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-24 15:19:10 +0300

    Added Chapter10's Checkpoints

---
+++ src/chapters/chapter_10/Checkpoints10/Checkpoint10_14.java
@@ -0,0 +1,12 @@
+package chapters.chapter_10.Checkpoints10;
+
+public class Checkpoint10_14 {
+    public static void main(String[] args) {
+        java.math.BigInteger x = new java.math.BigInteger("3");
+        java.math.BigInteger y = new java.math.BigInteger("7");
+        java.math.BigInteger z = x.add(y);
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+        System.out.println("z is " + z);
+    }
+}
