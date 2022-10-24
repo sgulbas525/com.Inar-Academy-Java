c7765978ebedf971234c4ab0506a4c7486f38269
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-24 15:18:34 +0300

    Added Chapter10's Checkpoints

---
+++ src/chapters/chapter_10/Checkpoints10/Checkpoint10_10_11.java
@@ -0,0 +1,28 @@
+package chapters.chapter_10.Checkpoints10;
+
+public class Checkpoint10_10_11 {
+    public static void main(String[] args) {
+
+        // Checkpoint10_10
+
+        Integer x = new Integer(3);
+        System.out.println(x.intValue());
+        System.out.println("x.intValue()" + x.compareTo(new Integer(4)));
+
+
+
+
+
+
+        // Checkpoint10_11
+
+        System.out.println("Integer.parseInt(\"10\") = " + Integer.parseInt("10"));
+        System.out.println("Integer.parseInt(\"10\", 10) = " + Integer.parseInt("10", 10));
+        System.out.println("Integer.parseInt(\"10\", 16) = " + Integer.parseInt("10", 16));
+        System.out.println("Integer.parseInt(\"11\") = " + Integer.parseInt("11"));
+        System.out.println("Integer.parseInt(\"11\", 10) = " + Integer.parseInt("11", 10));
+        System.out.println("Integer.parseInt(\"11\", 16)) = " + Integer.parseInt("11", 16));
+
+
+    }
+}
