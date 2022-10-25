c136dffdac4db70ddf2df5891380f95df4748518
Serhat Gulbas <sgulbas525@gmail.com>
2022-10-25 20:33:22 +0300

    Revised Exercise08_10

--- src/chapters/chapter_08/Exercise08_15.java
+++ src/chapters/chapter_08/Exercise08_15.java
@@ -29,20 +29,14 @@ public class Exercise08_15 {
         double y0 = points[0][1];
         double x1 = points[1][0];
         double y1 = points[1][1];
-        double x2 = points[2][0];
-        double y2 = points[2][1];
-        double x3 = points[3][0];
-        double y3 = points[3][1];
-        double x4 = points[4][0];
-        double y4 = points[4][1];
 
-        // Whether first three points are on the same line
-        double loc1 = ((x1 - x0) * (y2 - y0)) - ((x2 - x0) * (y1 - y0));
-        //Whether last three points are on the same line
-        double loc2 = ((x3 - x2) * (y4 - y2)) - ((x4 - x2) * (y3 - y2));
+        for (int i = 2; i < points.length; i++) {
+            if (((x1 - x0) * (points[i][1] - y0)) - ((points[i][0] - x0) * (y1 - y0)) != 0)
+                return false;
+        }
 
 
-        return (loc1 == 0 && loc2 == 0);
+        return true;
     }
 
 }
