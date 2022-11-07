dbb59a94048496839238020aeb6be309a721722e
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-07 23:58:25 +0200

    Added some checkpoints of chapter11

---
+++ src/chapters/chapter_11/Checkpoints11/Checkpoint11_01_02_03.java
@@ -0,0 +1,25 @@
+package chapters.chapter_11.Checkpoints11;
+
+public class Checkpoint11_01_02_03 {
+
+
+    // Checkpoint11_01
+
+    /* A subclass is not  a subset of a superclass */
+
+
+
+    // Checkpoint11_02
+
+    /* For defining a subclass we use 'extends' keyword */
+
+
+
+    // Checkpoint11_03
+
+    /*
+    Single inheritance : means a class that have just one superclass
+    Multiple inheritance : means that class which has more than one superclass
+    Java doesn't support multiple inheritance.
+     */
+}
