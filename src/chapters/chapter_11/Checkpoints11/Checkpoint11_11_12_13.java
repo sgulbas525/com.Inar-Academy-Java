368ebc2cfff1c0e26734b11c34d334d820dd4491
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-07 23:59:46 +0200

    Added some checkpoints of chapter11

---
+++ src/chapters/chapter_11/Checkpoints11/Checkpoint11_11_12_13.java
@@ -0,0 +1,57 @@
+package chapters.chapter_11.Checkpoints11;
+
+public class Checkpoint11_11_12_13 {
+
+    // Checkpoint11_11
+
+    /*
+    public class Circle {
+        private double radius;
+
+        public Circle(double radius) {
+        this.radius = radius;
+        }
+
+        public double getRadius() {
+        return radius;
+        }
+
+        public double getArea() {
+            return radius * radius * Math.PI;
+        }
+    }
+
+    class B extends Circle {
+        private double length;
+
+        B(double radius, double length) {
+            super(radius);
+            this.length = length;
+        }
+
+        @Override
+        public double getArea() {
+        return getArea() * length;
+        }
+    }
+     */
+
+
+
+    // Checkpoint11_12
+
+    /*
+    Overridden: These methods the method signature and the return type is the same.
+        And these methods are in different classes related by inheritance.
+
+    Overloaded: These methods have same name but different parameter lists. And these
+        methods can be either in same class or different classes related by inheritance
+     */
+
+
+    //Checkpoint11_13
+
+    /*
+    A method that has same signature and same return type with its superclass method's, that method is overridden
+     */
+}
