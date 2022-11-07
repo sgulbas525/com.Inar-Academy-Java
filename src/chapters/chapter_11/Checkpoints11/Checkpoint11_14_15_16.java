a80528446bfa56f7965ba3d94e5cb933fd13b791
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-08 00:00:06 +0200

    Added some checkpoints of chapter11

---
+++ src/chapters/chapter_11/Checkpoints11/Checkpoint11_14_15_16.java
@@ -0,0 +1,28 @@
+package chapters.chapter_11.Checkpoints11;
+
+public class Checkpoint11_14_15_16 {
+
+    // Checkpoint11_14
+
+    /*
+    There will be a problem if it's return type isn't a compatible type.
+     */
+
+
+
+    // Checkpoint11_15
+
+    /*
+    A method has the same signature but different parameters with it's superclass method is overloaded.
+     */
+
+
+
+    // Checkpoint11_16
+
+    /*
+    @Override annotation denotes that the annotated method is required to override a method int the superlcass.
+     If a method with this annotation does not override its superclass's method,
+      the compiler will report an error.
+     */
+}
