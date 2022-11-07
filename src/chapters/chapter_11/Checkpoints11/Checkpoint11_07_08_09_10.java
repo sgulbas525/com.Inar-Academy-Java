8598c27f71c3df645ac9cb6cd214107426dde27e
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-07 23:59:22 +0200

    Added some checkpoints of chapter11

---
+++ src/chapters/chapter_11/Checkpoints11/Checkpoint11_07_08_09_10.java
@@ -0,0 +1,33 @@
+package chapters.chapter_11.Checkpoints11;
+
+public class Checkpoint11_07_08_09_10 {
+
+    // Checkpoint11_07
+
+    /*
+    False. To overriding a method that method ust be accessible outside its own class.
+     */
+
+
+    // Checkpoint11_08
+
+    /*
+    False. A static method cannot be overridden.
+     */
+
+
+    // Checkpoint11_09
+
+    /*
+    By using super() or super(args) and that keyword must be the first statement.
+     */
+
+
+    // Checkpoint11_10
+
+    /*
+    super.MethodName() or super.MethodName(args)
+     */
+
+
+}
