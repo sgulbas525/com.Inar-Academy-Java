da8db921a79f1c9ced1286d42885b24ca0b17289
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-07 23:58:56 +0200

    Added some checkpoints of chapter11

---
+++ src/chapters/chapter_11/Checkpoints11/Checkpoint11_05_06.java
@@ -0,0 +1,23 @@
+package chapters.chapter_11.Checkpoints11;
+
+public class Checkpoint11_05_06 {
+
+    // Checkpoint11_05
+
+    /*
+       A subclass can explicitly invoke its superclass's constructor by using 'super' keyword
+     */
+
+
+    // Checkpoint11_06
+
+    /*
+    False. If a subclass's constructor explicitly invoke a superclass's constructor,
+     the superclass's no-arg constructor is not invoked.
+     */
+
+
+
+
+
+}
