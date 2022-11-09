a5dad9bd240346b5b79dbc1b53b80638148a17c2
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-09 14:17:25 +0200

    Added checkpoints of chapter11

---
+++ src/chapters/chapter_11/Checkpoints11/Checkpoint11_41_42.java
@@ -0,0 +1,23 @@
+package chapters.chapter_11.Checkpoints11;
+
+public class Checkpoint11_41_42 {
+
+    // Checkpoint11_41
+
+    /*
+     By using final keyword for the class can prevent the class to extend.
+     By using final keyword for the method can prevent the method to override.
+     */
+
+
+    // Checkpoint11_42
+
+    /*
+a. A protected datum or method can be accessed by any class in the same package.-----> true
+b. A protected datum or method can be accessed by any class in different packages.---> false
+c. A protected datum or method can be accessed by its subclasses in any package.-----> true
+d. A final class can have instances.-------------------------------------------------> true
+e. A final class can be extended.----------------------------------------------------> false
+f. A final method can be overridden.-------------------------------------------------> false
+     */
+}
