5b59892ff52f443966b234b1aca43b6ec6ba8a56
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-09 14:14:03 +0200

    Added checkpoints of chapter11

---
+++ src/chapters/chapter_11/Checkpoints11/Checkpoint11_33_34.java
@@ -0,0 +1,27 @@
+package chapters.chapter_11.Checkpoints11;
+
+public class Checkpoint11_33_34 {
+
+    // Checkpoint11_33
+    /*
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.remove(1);
+    System.out.println(list);
+    ArrayList has two overloaded method named remove. One removes object at specified index the other one removes the Object
+    Because it removes at the 1 index not the Element that's values is 1
+     */
+
+
+
+    // Checkpoint11_34
+    /*
+    ArrayList<Double> list = new ArrayList<>();
+    list.add(1);
+
+    list consists of Double objects. 1 will autoboxing, and becomes an Integer object
+     so that will wrong to try to add an Integer Object into a Double list
+     */
+}
