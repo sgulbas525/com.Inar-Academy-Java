39ebe09a72fa48ca47c33cbcd564adea16f515de
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-09 14:16:15 +0200

    Added checkpoints of chapter11

---
+++ src/chapters/chapter_11/Checkpoints11/Checkpoint11_35_36.java
@@ -0,0 +1,19 @@
+package chapters.chapter_11.Checkpoints11;
+
+public class Checkpoint11_35_36 {
+
+    // Checkpoint11_35
+    /*
+    int[] array = {3, 5, 95, 4, 15, 34, 3, 6, 5};
+    ArrayList<Integer> list = new ArrayList<>(Arrays.asList(array));
+    Array must be an array of objects
+     */
+
+
+    // Checkpoint11_36
+    /*
+    int[] array = {3, 5, 95, 4, 15, 34, 3, 6, 5};
+    System.out.println(java.util.Collections.max(array));
+    to use Collection.max(array) array must be used asList(array)
+     */
+}
---
+++ src/chapters/chapter_11/Checkpoints11/Checkpoint11_37_38.java
@@ -0,0 +1,15 @@
+package chapters.chapter_11.Checkpoints11;
+
+public class Checkpoint11_37_38 {
+
+    // Checkpoint11_37
+    /*
+    default visibility modifier should be used
+     */
+
+
+    // Checkpoint11_38
+    /*
+    protected visibility modifier should be used
+     */
+}
