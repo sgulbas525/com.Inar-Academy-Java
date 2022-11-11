59e0970d4ff342c2d68345b216aecae29629e593
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-11 13:26:25 +0200

    Added exercises of chapter11

---
+++ src/chapters/chapter_11/Exercises11/E13/Exercise11_13.java
@@ -0,0 +1,28 @@
+package chapters.chapter_11.Exercises11.E13;
+
+import java.util.ArrayList;
+
+import static chapters.chapter_11.Exercises11.E11.Exercise11_11.getFromUser;
+
+public class Exercise11_13 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = getFromUser(10);
+
+        removeDuplicates(list);
+        System.out.println("The distinct integers are ");
+        System.out.println(list);
+    }
+
+    public static void removeDuplicates(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).equals(list.get(j))){
+                    // When we remove one element in our list the size will decrease by one automatically,
+                    // the elements will shift left by one, so we will skip one element without compare.
+                    // To prevent that decrease the index follower variable by one when we remove an element.
+                    list.remove(j--);
+                }
+            }
+        }
+    }
+}
