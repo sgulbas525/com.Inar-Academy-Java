42c3f3db4ed06f0bbcd49de60dcbcb7c9af1ce9e
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-14 12:40:26 +0200

    Added some Checkpoints of chapter12

---
+++ src/chapters/chapter_12/Checkpoints12/Checkpoint12_01_02_03_04.java
@@ -0,0 +1,30 @@
+package chapters.chapter_12.Checkpoints12;
+
+public class Checkpoint12_01_02_03_04 {
+
+    //Checkpoint12_01
+    /*
+        It enables a method to throw an exception and
+       handle with that outside the method and that makes the code more precises
+     */
+
+    // Checkpoint12_02
+    /*
+    System.out.println(1 / 0); ------> will thrown an exception
+    System.out.println(1.0 / 0);-----> won't throw any exception
+     */
+
+
+    // Checkpoint12_03
+    /*
+    long value = Long.MAX_VALUE + 1;
+    System.out.println(value);
+    just exceed it's maximum limits, won't throw any exception
+     */
+
+
+    //Checkpoint12_04
+    /*
+    In an exception occurs, JVM starts for looking a catch block.
+     */
+}
