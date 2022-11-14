3e28c9de19ef1ad931bfb8b960a502e191437f48
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-14 17:41:43 +0200

    Added some Listings of chapter12

---
+++ src/chapters/chapter_12/Listings12/Listing12_11_TestCircleWithCustomException.java
@@ -0,0 +1,57 @@
+package chapters.chapter_12.Listings12;
+
+public class Listing12_11_TestCircleWithCustomException {
+    public static void main(String[] args) {
+        try {
+            new CircleWithCustomException(5);
+            new CircleWithCustomException(-5);
+            new CircleWithCustomException(0);
+        } catch (Listing11_09_InvalidRadiusException ex) {
+            System.out.println(ex);
+        }
+
+        System.out.println("Number of objects created: " +
+                CircleWithCustomException.getNumberOfObjects());
+    }
+}
+
+class CircleWithCustomException {
+    // The radius of the circle
+    private double radius;
+
+    // The number of objects created
+    private static int numberOfObjects = 0;
+
+    // Construct a circle with radius 1
+    public CircleWithCustomException() throws Listing11_09_InvalidRadiusException {
+        this(1.0);
+    }
+
+    // Construct a circle with a specified radius
+    public CircleWithCustomException(double newRadius) throws Listing11_09_InvalidRadiusException {
+        setRadius(newRadius);
+        numberOfObjects++;
+    }
+
+    // Return radius
+    public double getRadius() {
+        return radius;
+    }
+
+    // Set a new radius
+    public void setRadius(double newRadius) throws Listing11_09_InvalidRadiusException {
+        if (newRadius >= 0)
+            this.radius = radius;
+        else
+            throw new Listing11_09_InvalidRadiusException(newRadius);
+    }
+
+    // Return numberOfObjects
+    public static int getNumberOfObjects() {
+        return numberOfObjects;
+    }
+
+    public double findArea() {
+        return radius * radius * Math.PI;
+    }
+}
