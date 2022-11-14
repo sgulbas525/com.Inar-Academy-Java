4683e2a7def15b3cd89d4af84889c3ca2914f2a5
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-14 12:17:45 +0200

    Revised E13

--- src/chapters/chapter_11/Exercises11/E13/Exercise11_13.java
+++ src/chapters/chapter_11/Exercises11/E13/Exercise11_13.java
@@ -14,15 +14,24 @@ public class Exercise11_13 {
     }
 
     public static void removeDuplicates(ArrayList<Integer> list) {
+        ArrayList<Integer> removed = new ArrayList<>();
+        while (!list.isEmpty()) {
+            int n = list.remove(list.size() - 1);
+            if (!removed.contains(n)) {
+                removed.add(n);
+            }
+        }
+        list.addAll(removed);
+        /* Another solution
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
                 if (list.get(i).equals(list.get(j))){
-                    // When we remove one element in our list the size will decrease by one automatically,
-                    // the elements will shift left by one, so we will skip one element without compare.
-                    // To prevent that decrease the index follower variable by one when we remove an element.
-                    list.remove(j--);
-                }
-            }
-        }
+                    When we remove one element in our list the size will decrease by one automatically,
+                    the elements will shift left by one, so we will skip one element without compare.
+                    To prevent that decrease the index follower variable by one when we remove an element.
+                    list.remove(j--);*/
     }
 }
+
+
+
