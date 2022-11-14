58342311ccbbfccd433f8cd79375ff42da1e5690
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-14 12:18:49 +0200

    Added some Listings of chapter12

---
+++ src/chapters/chapter_12/Listings12/Listing_02_QuotientWithIf.java
@@ -0,0 +1,22 @@
+package chapters.chapter_12.Listings12;
+
+import java.util.Scanner;
+
+public class Listing_02_QuotientWithIf {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter two integers
+        System.out.print("Enter two integers: ");
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+
+        if (number2 != 0) {
+            System.out.println(number1 + " / " + number2 + " is " +
+                    (number1 / number2));
+        } else {
+            System.out.println("Divisor cannot be zero!");
+        }
+
+    }
+}
