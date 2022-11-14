2d96ac53b99310f3e296f974a76b7c8d5f19247e
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-14 12:19:41 +0200

    Added some Listings of chapter12

---
+++ src/chapters/chapter_12/Listings12/Listing_05_InputMismatchExceptionDemo.java
@@ -0,0 +1,27 @@
+package chapters.chapter_12.Listings12;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Listing_05_InputMismatchExceptionDemo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean continueInput = true;
+
+        do {
+            try {
+                System.out.print("Enter an integer: ");
+                int number = input.nextInt();
+
+                //Display the result
+                System.out.println("The number entered is " + number);
+
+                continueInput = false;
+            } catch (InputMismatchException ex) {
+                System.out.println("Try again!  (Incorrect input: )" +
+                        "an integer is required.");
+                input.nextLine();
+            }
+        } while (continueInput);
+    }
+}
