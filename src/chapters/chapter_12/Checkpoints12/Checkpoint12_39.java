c9d25b340e4bdf3a2173df5d755d51cc5f194b65
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-15 00:30:37 +0200

    Added some Checkpoints of chapter12

---
+++ src/chapters/chapter_12/Checkpoints12/Checkpoint12_39.java
@@ -0,0 +1,7 @@
+package chapters.chapter_12.Checkpoints12;
+
+public class Checkpoint12_39 {
+    /*
+    If the link in the pendingURL include the same link again there will be duplicate.
+     */
+}
