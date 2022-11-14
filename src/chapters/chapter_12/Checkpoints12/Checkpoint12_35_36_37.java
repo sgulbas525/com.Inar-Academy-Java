095c6e17f6b3b8d281f4201ec98d0017183994a0
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-15 00:03:02 +0200

    Added some Checkpoints of chapter12

---
+++ src/chapters/chapter_12/Checkpoints12/Checkpoint12_35_36_37.java
@@ -0,0 +1,40 @@
+package chapters.chapter_12.Checkpoints12;
+
+public class Checkpoint12_35_36_37 {
+
+
+    // Checkpoint12_35
+    /*
+    No. The line separator on Windows is \r\n
+     */
+
+
+
+    // Checkpoint12_36
+   /*
+    Scanner input = new Scanner(System.in);
+    int intValue = input.nextInt();
+    double doubleValue = input.nextDouble();
+    String line = input.nextLine();
+
+    Output:
+    intValue =45
+    doubleValue = 57.8
+    line = _789
+    */
+
+
+
+    // Checkpoint12_37
+   /*
+    Scanner input = new Scanner(System.in);
+    int intValue = input.nextInt();
+    double doubleValue = input.nextDouble();
+    String line = input.nextLine();
+
+    Output:
+    intValue =45
+    doubleValue = 57.8
+    line = (empty)
+    */
+}
