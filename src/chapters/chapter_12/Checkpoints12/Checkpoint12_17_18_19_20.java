8c0bd15d569095786687a12dd44710c7fb14e375
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-14 14:04:53 +0200

    Added some Checkpoints of chapter12

---
+++ src/chapters/chapter_12/Checkpoints12/Checkpoint12_17_18_19_20.java
@@ -0,0 +1,34 @@
+package chapters.chapter_12.Checkpoints12;
+
+public class Checkpoint12_17_18_19_20 {
+
+    // Checkpoint12_17
+    /*
+    getMessage(): returns a String that describes the exception
+     */
+
+
+    // Checkpoint12_18
+    /*
+    printStackTrace(): display the trace information
+     */
+
+
+
+    // Checkpoint12_19
+    /*
+    No
+     */
+
+
+
+
+    // Checkpoint12_20
+    /*
+    public void m(int value) throws Exception {
+        if (value < 40)
+            throw new Exception("value is too small");
+    }
+
+     */
+}
