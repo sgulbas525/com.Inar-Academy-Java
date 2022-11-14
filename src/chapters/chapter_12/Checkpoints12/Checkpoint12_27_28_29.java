8fa25d46c1ddf589f71a3f594a43dc6e362472b2
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-14 21:25:34 +0200

    Added some Listings of chapter12

---
+++ src/chapters/chapter_12/Checkpoints12/Checkpoint12_27_28_29.java
@@ -0,0 +1,32 @@
+package chapters.chapter_12.Checkpoints12;
+
+public class Checkpoint12_27_28_29 {
+
+    // Checkpoint12_17
+    /*
+    new File("c:\book\test.dat");
+      \ is a special character so if you want to separate directories
+       you have to use \\ for writing \.
+     */
+
+
+
+
+    // Checkpoint12_28
+    /*
+    File file = new File("c:\\blablabla\\Blabla")
+    file.exists();
+    file.delete();
+    file.renameTO(File f);
+    file.length();
+    file.mkdirs()
+     */
+
+
+
+    // Checkpoint12_29
+    /*
+    You cannot use File class for I/O
+    File object doesn't create a file on the disk
+     */
+}
