2d91e5a12484a8639d316d738c4458d633d81a6c
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-14 14:22:21 +0200

    Added some Checkpoints of chapter12

---
+++ src/chapters/chapter_12/Checkpoints12/Checkpoint12_21.java
@@ -0,0 +1,24 @@
+package chapters.chapter_12.Checkpoints12;
+
+public class Checkpoint12_21 {
+    /*
+    try {
+        statement1;
+        statement2;
+        statement3;
+    }
+    catch (Exception1 ex1) {
+    }
+    finally {
+        statement4;
+    }
+    statement5;
+    }
+   No exception occurs;
+        statement4: Yes, statement5: Yes.
+   Exception1 occurs;
+        statement4: Yes, statement5: No.
+   Exception occurs but not Exception1:
+        statement4: Yes, statement5: No.
+
+     */
