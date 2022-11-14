baca929bdfa0e5202e2f5f96e8461ce4f7347adb
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-14 12:57:46 +0200

    Added some Checkpoints of chapter12

---
+++ src/chapters/chapter_12/Checkpoints12/Checkpoint12_07.java
@@ -0,0 +1,9 @@
+package chapters.chapter_12.Checkpoints12;
+
+public class Checkpoint12_07 {
+    /*
+    Throwable is the root class of Javas exception classes.It has 2 subclass Errors and Exception.
+    Error: describes fatal system errors.
+    Exception: describes the errors that can be handled by java programs.
+     */
+}
