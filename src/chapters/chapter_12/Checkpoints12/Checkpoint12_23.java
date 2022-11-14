36558799d1a38697c05f7dcd39a848a9d6b9c450
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-14 17:04:32 +0200

    Added some Checkpoints of chapter12

---
+++ src/chapters/chapter_12/Checkpoints12/Checkpoint12_23.java
@@ -0,0 +1,32 @@
+package chapters.chapter_12.Checkpoints12;
+
+public class Checkpoint12_23 {
+
+    // Checkpoint12_23
+    /*
+    try {
+        statement1;
+        statement2;
+        statement3;
+    }
+    catch (Exception1 ex1) {
+    }
+    catch (Exception2 ex2) {
+        throw ex2;
+    }
+    finally {
+        statement4;
+    }
+    statement5;
+}
+No exception;
+-statement4: Yes, statement5: Yes
+Exception1;
+-statement4: Yes, statement5: Yes
+Exception2;
+-statement4: Yes, statement5: No
+Exception1;
+-statement4: Yes, statement5: No
+
+*/
+}
