1006766d7b5ef552037c2540f18fe68f1aa78c09
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-14 16:52:27 +0200

    Added some Checkpoints of chapter12

---
+++ src/chapters/chapter_12/Checkpoints12/Checkpoint12_22.java
@@ -0,0 +1,25 @@
+package chapters.chapter_12.Checkpoints12;
+
+public class Checkpoint12_22 {
+
+    // Checkpoint12_22
+   /* public static boolean isNumeric(String token) {
+        try {
+            Double.parseDouble(token);
+            return true;
+        }
+        catch (java.lang.NumberFormatException ex) {
+            return false;
+        }
+    }
+    It's correct
+
+    public static boolean isNumeric(String token) {
+
+        for(int i = 0; i < token.length(); i++)
+            if('9' < token.charAt(i) && token.charAt(i) < '0')
+                return false;
+
+        return true;
+    */
+}
