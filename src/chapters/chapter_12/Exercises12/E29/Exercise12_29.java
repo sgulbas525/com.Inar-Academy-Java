222ad77e31aad403e4c5105d3f7694f8ede33e3f
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-18 12:14:40 +0200

    Added some exercises of chapter12

---
+++ src/chapters/chapter_12/Exercises12/E29/Exercise12_29.java
@@ -0,0 +1,18 @@
+package chapters.chapter_12.Exercises12.E29;
+
+import java.io.File;
+
+public class Exercise12_29 {
+    public static void main(String[] args) {
+        for (String s : args) {
+            File file = new File(s);
+            StringBuilder sb1 = new StringBuilder(s);
+            if (s.matches("Exercise\\d_\\d")) {
+                int n = s.indexOf("_");
+                sb1.insert(n + 1, 0);
+            }
+            File newName = new File(String.valueOf(sb1));
+            file.renameTo(newName);
+        }
+    }
+}
