1cdc447386fc32ee75f22dfa1cdbbc45faac0445
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-28 23:19:40 +0200

    Added some Checkpoints of chapter13

---
+++ src/chapters/chapter_13/Checkpoints13/Checkpoints13_01_02_03.java
@@ -0,0 +1,37 @@
+package chapters.chapter_13.Checkpoints13;
+
+public class Checkpoints13_01_02_03 {
+
+    // Checkpoint13_01
+
+    /*
+    A ------> Illegal
+    B ------> Illegal
+    C ------> Illegal
+    E ------> Legal
+    D ------> Illegal
+    F ------> Legal
+     */
+
+
+    // Checkpoint13_02
+    /*
+    We can use them while compilation time and that makes compiling without errors.
+     */
+
+
+
+
+
+
+    // Checkpoint13_03
+
+    /*
+    - An abstract class can be used just like a nonabstract class except that you cannot
+        use the new operator to create an instance from the abstract class. ------> True
+    - An abstract class can be extended. -----> True
+    - A subclass of a nonabstract superclass cannot be abstract -----> False
+    - A subclass cannot override a concrete method in a superclass to define it as abstract. -----> False
+    - An abstract method must be nonstatic. -----> True
+     */
+}
