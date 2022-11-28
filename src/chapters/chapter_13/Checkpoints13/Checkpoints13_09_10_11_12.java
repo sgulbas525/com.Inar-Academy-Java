162b8f21d92393205ee2478b77bb7b262e7f5304
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-28 23:48:13 +0200

    Added some Checkpoints of chapter13

---
+++ src/chapters/chapter_13/Checkpoints13/Checkpoints13_09_10_11_12.java
@@ -0,0 +1,33 @@
+package chapters.chapter_13.Checkpoints13;
+
+public class Checkpoints13_09_10_11_12 {
+
+    // Checkpoint13_09
+    /*
+    No, you can't. Because Calendar class is an abstract class
+     */
+
+
+    // Checkpoint13_10
+    /*
+    The add method is an abstract method
+     */
+
+
+    // Checkpoint13_11
+    /*
+    Calendar currentTime = ne GregorianCalender();
+     */
+
+
+
+    // Checkpoint13_12
+    /*
+    c.get(Calender.YEAR);
+    c.get(Calender.MONTH);
+    c.get(Calender.DAY_OF_MONTH);
+    c.get(Calender.HOUR);
+    c.get(Calender.MINUTE);
+    c.get(Calender.SECOND);
+     */
+}
