6063004139e64720bdb56ed849615f0028cc1d85
Serhat Gulbas <sgulbas525@gmail.com>
2022-11-29 00:43:45 +0200

    Added some listings of chapter13

---
+++ src/chapters/chapter_13/Listings13/Listing13_08_SortComparableObjects.java
@@ -0,0 +1,21 @@
+package chapters.chapter_13.Listings13;
+
+import java.math.BigInteger;
+
+public class Listing13_08_SortComparableObjects {
+    public static void main(String[] args) {
+        String[] cities = {"Savannah", "Boston", "Atlanta","Tampa"};
+        java.util.Arrays.sort(cities);
+        for (String city: cities) {
+            System.out.print(city + " ");
+        }
+        System.out.println();
+
+        BigInteger[] hugeNumbers = {new BigInteger("232323231092923992"),
+                new BigInteger("432232323239292"), new BigInteger("54623239292")};
+        java.util.Arrays.sort(hugeNumbers);
+        for (BigInteger number: hugeNumbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
